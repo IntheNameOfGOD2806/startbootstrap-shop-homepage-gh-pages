7b23a5c76733d6182c04db8810f56d04f78bb7f7
Trần thành Đạt <101255344+IntheNameOfGOD2806@users.noreply.github.com>
2024-04-19 18:36:19 +0700

    commit

--- vite-project/src/components/footer/Footer.jsx
+++ vite-project/src/components/footer/Footer.jsx
@@ -1,7 +1,19 @@
 
 const Footer = () => {
     return (
-      <div>Footer</div>
+      <div><>
+      {/* Footer*/}
+      <footer className="py-5 bg-dark">
+        <div className="container">
+          <p className="m-0 text-center text-white">
+            Copyright © Your Website 2023
+          </p>
+        </div>
+      </footer>
+      {/* Bootstrap core JS*/}
+      {/* Core theme JS*/}
+    </>
+    </div>
     )
   }
   
